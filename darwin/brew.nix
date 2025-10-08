aeef40a2ce49493c9c03363c699dadb2c798b92c
Iso <vetle.olstad@gmail.com>
2025-10-08 21:00:20 +0200

    Comment out html2markdown tap in Homebrew configuration and Dark Reader for Safari in mas.nix

--- darwin/brew.nix
+++ darwin/brew.nix
@@ -3,5 +3,5 @@ _:
 [
   "ghostscript"
   #"tailscale"
-  "JohannesKaufmann/tap/html2markdown"
+  #"JohannesKaufmann/tap/html2markdown"
 ]
--- darwin/mas.nix
+++ darwin/mas.nix
@@ -3,5 +3,6 @@
   "SponsorBlock for Safari" = 1573461917;
   "Ghostery Privacy Ad Blocker" = 6504861501;
   "1Password for Safari" = 1569813296;
-  "Dark Reader for Safari" = 1438243180;
+  #"Dark Reader for Safari" = 1438243180;
+  "Noir Dark Mode for Safari" = 1592917505;
 }
