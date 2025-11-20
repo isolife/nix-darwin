559fa354819048a4fe60c53bd3367e03f30976f2
Iso <vetle.olstad@gmail.com>
2025-11-20 15:23:55 +0100

    Comment out Ghostery Privacy Ad Blocker in mas.nix

--- darwin/mas.nix
+++ darwin/mas.nix
@@ -1,7 +1,7 @@
 {
   "uBlock Origin Lite" = 6745342698;
   "SponsorBlock for Safari" = 1573461917;
-  "Ghostery Privacy Ad Blocker" = 6504861501;
+  #"Ghostery Privacy Ad Blocker" = 6504861501;
   "1Password for Safari" = 1569813296;
   #"Dark Reader for Safari" = 1438243180;
   "Noir Dark Mode for Safari" = 1592917505;
