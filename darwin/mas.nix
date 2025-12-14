b1f4e90c9cd03dd76ae966909fc83e7505021e40
Iso <vetle.olstad@gmail.com>
2025-12-14 13:54:04 +0100

    Comment out uBlock Origin Lite and add AdGuard Mini in mas.nix

--- darwin/mas.nix
+++ darwin/mas.nix
@@ -1,8 +1,9 @@
 {
-  "uBlock Origin Lite" = 6745342698;
+  #"uBlock Origin Lite" = 6745342698;
   "SponsorBlock for Safari" = 1573461917;
   #"Ghostery Privacy Ad Blocker" = 6504861501;
   "1Password for Safari" = 1569813296;
   #"Dark Reader for Safari" = 1438243180;
   "Noir Dark Mode for Safari" = 1592917505;
+  "AdGuard Mini" = 1440147259;
 }
